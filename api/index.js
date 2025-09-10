9b38de71b51ff9149d3fa3197d7e5fd7910ba8ae
Md Rizwanul Haque <87943114+rizwanhrizvi@users.noreply.github.com>
2025-09-11 00:27:26 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,14 +7,14 @@ export default async function handler(req, res) {
   // Log the request
   console.log("Request received:", { method, url, headers });
 
-  const logUrl = `https://script.google.com/macros/s/AKfycbx3Q5Yi5NghybFhyXyHTmmKNyZqhGgyoJcV0anYFPdsh9N1jRSEDgRIwsWrVWaQJc4/exec?page=${encodeURIComponent(query.page || "unknown")}&ua=${encodeURIComponent(headers["user-agent"] || "unknown")}&ip=${encodeURIComponent(headers["x-forwarded-for"] || req.socket.remoteAddress || "unknown")}`;
+  // const logUrl = `https://script.google.com/macros/s/AKfycbx3Q5Yi5NghybFhyXyHTmmKNyZqhGgyoJcV0anYFPdsh9N1jRSEDgRIwsWrVWaQJc4/exec?page=${encodeURIComponent(query.page || "unknown")}&ua=${encodeURIComponent(headers["user-agent"] || "unknown")}&ip=${encodeURIComponent(headers["x-forwarded-for"] || req.socket.remoteAddress || "unknown")}`;
 
-  // Call Google Script via GET
-  try {
-      await fetch(logUrl);
-  } catch (err) {
-      console.error("Failed to log to Google Sheets:", err);
-  }
+  // // Call Google Script via GET
+  // try {
+  //     await fetch(logUrl);
+  // } catch (err) {
+  //     console.error("Failed to log to Google Sheets:", err);
+  // }
 
   // Serve the HTML
   const filePath = path.join(process.cwd(), "public", "site.html");
