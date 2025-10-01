d1b67ddacdfaa1a16b47663705d3d9f112966c47
Md Rizwanul Haque <87943114+rizwanhrizvi@users.noreply.github.com>
2025-10-01 13:58:58 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,7 +5,10 @@ export default async function handler(req, res) {
   const { method, query, url, headers } = req;
 
   // Log request
-  console.log("Request received:", { method, query, url, headers });
+  // console.log("Request received:", { method, query, url, headers });
+
+  console.log("Request received:", req);
+  console.log("Response sent:", res);
 
   const logUrl = `https://script.google.com/macros/s/AKfycbx3Q5Yi5NghybFhyXyHTmmKNyZqhGgyoJcV0anYFPdsh9N1jRSEDgRIwsWrVWaQJc4/exec?page=${encodeURIComponent(query.page || "unknown")}&ua=${encodeURIComponent(headers["user-agent"] || "unknown")}&ip=${encodeURIComponent(headers["x-forwarded-for"] || req.socket.remoteAddress || "unknown")}`;
 
